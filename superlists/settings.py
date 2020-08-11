df719bbc8627923f25d4f679da99f62b54b993d1
Omulosi <mulongojohnpaul@gmail.com>
2020-08-11 15:03:54 +0300

    fix hacked ALLOWED HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yc_4ilf1kj0_4aq%^nu+d0sgdx4w7xdzu@$#y@j=$4rls4o3yf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
